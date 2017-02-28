734124d09f1f45f74d70aed1e47135666414d34b
ninjaofdeath30 <ninjaofdeath30@gmail.com>
2017-02-27 22:32:40 -0700

    angular and application user api started. Also some layout changes...

--- HCTakeHome/App_Start/BundleConfig.cs
+++ HCTakeHome/App_Start/BundleConfig.cs
@@ -26,6 +26,10 @@ namespace HCTakeHome
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angularjs").Include(
+                "~/Scripts/angular.min.js",
+                "~/Scripts/app.js"));
         }
     }
 }
--- HCTakeHome/Models/IdentityModels.cs
+++ HCTakeHome/Models/IdentityModels.cs
@@ -29,5 +29,7 @@ namespace HCTakeHome.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<HCTakeHome.Models.ApplicationUser> ApplicationUsers { get; set; }
     }
 }
